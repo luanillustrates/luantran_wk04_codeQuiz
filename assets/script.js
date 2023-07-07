7bcdcc81ca101fb22907c9e62257236d0fe32e7f
luanillustrates <luanillustrates@gmail.com>
2023-07-07 14:58:44 +0930

    complete html, some css, some js added

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var questionSet = [
+    { question: 'Where do you link the JavaScipt in the HTML?',
+    choices: ["1. it doesn't", "2. in the <head>", "3. in the <body>", "4. inbetween the <head> and <body>"],
+    answer: "3. in the <body>",
+    },
+
+    {question: '.querytselector() selects which specified element?',
+    choices: ["1. all", "2. first", "3. last", "4. none"],
+    answer: "2. first",
+    },
+
+    {question: 'Which bracket sets do you use for an array?',
+    choices: ["1. []", "2. ()", "3. {}", "4. ''"],
+    answer: "1. []",
+    },
+
+    {question: 'How many letters does JavaScript have?',
+    choices: ["1. 5", "2. 7", "3. 8", "4. 10"],
+    answer: "4. 10",
+    },
+
+    {question: 'DOM stands for?...',
+    choices: ["1. Document Object Model", "2. Dirty Old Men", "3. Document Obligatory Manuscripts", "4. Dual Overview Model"],
+    answer: "1. Document Object Model",
+    },
+]
+
+var highScores = [];
