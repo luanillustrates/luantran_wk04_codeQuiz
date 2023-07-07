1a5940334ae8a2ccb67197e8845e5f5f5e8997a6
luanillustrates <luanillustrates@gmail.com>
2023-07-07 18:17:04 +0930

    added more js code

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,122 @@
 var questionSet = [
-    { question: 'Where do you link the JavaScipt in the HTML?',
+    { questionOpt: 'Where do you link the JavaScipt in the HTML?',
     choices: ["1. it doesn't", "2. in the <head>", "3. in the <body>", "4. inbetween the <head> and <body>"],
     answer: "3. in the <body>",
     },
 
-    {question: '.querytselector() selects which specified element?',
+    {questionOpt: '.querytselector() selects which specified element?',
     choices: ["1. all", "2. first", "3. last", "4. none"],
     answer: "2. first",
     },
 
-    {question: 'Which bracket sets do you use for an array?',
+    {questionOpt: 'Which bracket sets do you use for an array?',
     choices: ["1. []", "2. ()", "3. {}", "4. ''"],
     answer: "1. []",
     },
 
-    {question: 'How many letters does JavaScript have?',
+    {questionOpt: 'How many letters does JavaScript have?',
     choices: ["1. 5", "2. 7", "3. 8", "4. 10"],
     answer: "4. 10",
     },
 
-    {question: 'DOM stands for?...',
+    {questionOpt: 'DOM stands for?...',
     choices: ["1. Document Object Model", "2. Dirty Old Men", "3. Document Obligatory Manuscripts", "4. Dual Overview Model"],
     answer: "1. Document Object Model",
     },
-]
+];
 
-var highScores = [];
+var startScreen = document.querySelector("#start-screen");
+var quizScreen = document.querySelector("#quiz-question");
+var finalScreen = document.querySelector("#finalscore");
+var leaderScreen = document.querySelector("#leaderboard");
+
+function hideContent() {
+    startScreen.setAttribute("hidden", true);
+    quizScreen.setAttribute("hidden", true);
+    finalScreen.setAttribute("hidden", true);
+    leaderScreen.setAttribute("hidden", true);
+};
+
+var result = document.querySelector("#result");
+var resultText = document.querySelector("#result-text");
+
+function hideResultText() {
+    result.style.display = "none";
+}
+
+var interval;
+var time;
+var currentQuestion;
+
+document.querySelector("#start-btn").addEventListener("click", startQuiz);
+
+function startQuiz() {
+    hideContent();
+    quizScreen.removeAttribute("hidden");
+
+    currentQuestion = 0;
+    displayQuestion();
+
+    time = 3000;
+
+    interval = setInterval(countdown, 1000);
+
+    displayTime();
+}
+
+function countdown() {
+    time--;
+    displayTime();
+    if (time < 1) {
+        endQuiz();
+    }
+}
+
+var showTime = document.querySelector("#timer");
+function displayTime() {
+    showTime.textContent = time;
+}
+
+function displayQuestion() {
+    var question = questions[currentQuestion];
+    var choices = question.choices;
+
+    var questionTitle = document.querySelector("#question-text");
+    questionTitle.textContent = question.questionOpt;
+
+    for (var i = 0; i < choices.length; i++) {
+        var choice = choices[i];
+        var choiceButton = document.querySelector("#choice" + i);
+        choiceButton.textContent = choice;
+    }
+}
+
+document.querySelector("#quiz-choices").addEventListener("click", checkResult);
+
+function correctChoice(choiceButton) {
+    return choiceButton.textContent === questions[currentQuestion].answer;
+}
+
+function checkResult(eventObject) {
+    var choiceButton = eventObject.target;
+    result.style.display = "block";
+
+    if (correctChoice(choiceButton)) {
+        resultText.textContent = "correct :)";
+        setTimeout(hideResultText, 1000);
+    } 
+    
+    else {
+        resultText.textContent = "incorrect :("
+        setTimeout(hideResultText, 1000);
+        if (time >= 10) {
+            time = time -10;
+            displayTime();
+        }
+        else {
+            time = 0;
+            displayTime();
+            endQuiz();
+        }
+    }
+}
